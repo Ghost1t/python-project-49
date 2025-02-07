7688075e575c2fbfb121498ed374e39d08f15aa9
Ilia_Pototskii <lor.lor@mail.ru>
2025-02-07 18:12:51 +0400

    3 stage

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
+import brain_games.cli
+
 def greet():
     print('Welcome to the Brain Games!')
 
 def main():
     greet()
+    brain_games.cli.welcome_user()
 
 if __name__ == "__name__":
     main()
